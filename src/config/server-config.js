8425e4aacdbd175f87e1b997c2d1e04af988a0dd
ANANT-RAI1101 <anantrai1101@gmail.com>
2025-12-27 17:49:00 +0530

    created the setup for message queue using rabbitmq , createchannel publish and consume

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -6,4 +6,6 @@ module.exports={
     PORT:process.env.PORT,
     EMAIL_ID: process.env.EMAIL_ID,
     EMAIL_PASS: process.env.EMAIL_PASS,
+    MESSAGE_BROKER_URL: process.env.MESSAGE_BROKER_URL,
+    EXCHANGE_NAME: process.env.EXCHANGE_NAME
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express=require('express');
 const bodyParser=require("body-parser");
-const{PORT}=require("./config/server-config")
-const ApiRoute=require("./routes/index")
+const{PORT}=require("./config/server-config");
+const ApiRoute=require("./routes/index");
 const job = require('./utils/jobs');
+const {createChannel}=require("./utils/message-queue");
 const setupAndRunServer=async()=>{
     const app=express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
-    app.use("/api",ApiRoute)
+    app.use("/api",ApiRoute);
+    const channel=await createChannel();
     app.listen(PORT,async()=>{
         console.log(`server is running at port ${PORT}`);
         job();    
